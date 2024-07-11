5d26f29e605a3d538c3305b1ec1dd7a656517da1
Abhilash107 <abhilash00003108884@gmail.com>
2024-07-11 14:09:30 +0530

    backend files

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import { Socket } from "dgram";
+import express from "express";
+import http from "http";
+import { Server } from 'socket.io';
+
+const app = express()
+
+const server = http.createServer(app)
+const PORT = 3000
+
+const io = new Server(server)
+
+io.on('connection', (socket) =>{
+   socket.on('user-message', (msg)=>{
+    console.log("A new user message", msg);
+    io.emit('message', msg)
+   })
+})
+
+app.use(express.static("../frontend"))
+
+app.get('/', (req, res) =>{
+    res.sendFile("../frontend/index.html")
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`); 
+})
